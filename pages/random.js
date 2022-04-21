d670f2b77dc14bcc448ead76f46e2d5bd24311d7
Saeed Nemati <sn.1361@gmail.com>
2022-04-21 10:46:14 +0430

    committed by script

--- pages/random.js
+++ pages/random.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react'
 import { videos as CognitoVideos } from "./cognito";
 import { videos as DaveVideos } from "./dave";
 import { videos as CrashVideos } from "./crash";
@@ -7,8 +8,18 @@ import { videos as DonVideos } from './don';
 
 const Cognito = ({ videoCode, from }) => {
 
-    return <div>
-        <a target='_blank' href={`https://youtube.com/watch?v=${videoCode}`}>Random Video ({videoCode})</a>
+    const link = useRef()
+
+    const reload = (e) => {
+        link.current.click()
+        setTimeout(() => {
+            window.location.reload()
+        }, 1000)
+        e.stopPropagation()
+        e.preventDefault()
+    }
+    return <div onClick={(e) => reload()} className="flex flex-col cursor-pointer w-screen h-screen items-center justify-center">
+        <a ref={link} target='_blank' href={`https://youtube.com/watch?v=${videoCode}`} className='text-5xl'>{videoCode}</a>
         <br />
         <div>from {from} videos</div>
     </div>
