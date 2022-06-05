8d8d2d6292ab2a94286ab4f851da10b25808c887
Saeed Nemati <sn.1361@gmail.com>
2022-06-05 22:03:42 +0430

    committed by script

--- pages/remove.js
+++ pages/remove.js
@@ -2,57 +2,30 @@ const fs = require('fs')
 const path = require('path')
 
 var codes = [
-    "LEN8FZEKaNU",
-    "sNWrOuwzax8",
-    "VeTeK9kvxyo",
-    "nGlQkaoIfBI",
-    "xLKUXI0enbg",
-    "P05G89hgrPI",
-    "jQpvph777Pg",
-    "b3mj4QQH8TM",
-    "kX0jHv05FYM",
-    "XdvIsJF9d9s",
-    "5VI3qcSuUlk",
-    "J1IyUPMXwS0",
-    "U96m8inKt24",
-    "snJpYLV7bYA",
-    "FS2ndY5WJXA",
-    "9m5I-HO3w8w",
-    "ZjAqfh9aY9Y",
-    "2_ktCNGYbrU",
-    "1xrcHQB5EEU",
-    "pDFiY81TLIM",
-    "fXRpTLvPdT0",
-    "mdvfXzCoqvA",
-    "5gxHbYI0uy4",
-    "oc3dWwbctw4",
-    "5lp-HBv-3bA",
-    "yzAdZDK4XKA",
-    "DenEXMVt0gU",
-    "zMO258I-7uc",
-    "wxzz31ww4M4",
-    "DA16n8ilUp8",
-    "M3K9uKIMob0",
-    "KMVnscTctqI",
-    "sDIvfEJ59XY",
-    "xiiaed3puhY",
-    "uMzuxuA1POU",
-    "jUM4RkZ1tuI",
-    "K3SzObknn8I",
-    "Fx0Z6y2HmlM",
-    "B99H6Ygnzgw",
-    "u_JmkmPYpec",
-    "sYJosVHkwgo",
-    "fDzVtXbtEow",
-    "vGLZDFHnnwk",
-    "DK_vZuLYHcw",
-    "c7fqMPDcKXM",
-    "eJ7w2I83ba4",
-    "qgajQD7PJp0",
-    "ovSW1G7wrCg",
-    "9sSLz5t7a5M",
-    "kn-ER4bL7f8",
-    "4kxzD4ASioI"
+    "qk0-m_LARCY",
+    "qk0-m_LARCY",
+    "VoZMOmjXENU",
+    "VoZMOmjXENU",
+    "tk4dIgIG5Qs",
+    "tk4dIgIG5Qs",
+    "gOp7tYFk-38",
+    "gOp7tYFk-38",
+    "98QmnyyIXsY",
+    "98QmnyyIXsY",
+    "bZBmOd83Fds",
+    "bZBmOd83Fds",
+    "2wRcDM_5rjs",
+    "2wRcDM_5rjs",
+    "bVMGyXnnoRM",
+    "bVMGyXnnoRM",
+    "NqDLB3JWfds",
+    "NqDLB3JWfds",
+    "Uxeqt6dIziQ",
+    "Uxeqt6dIziQ",
+    "GVNy3x_PwFk",
+    "GVNy3x_PwFk",
+    "if36bqHypqk",
+    "if36bqHypqk",
 ]
 
 const Remove = ({ content }) => {
@@ -75,9 +48,9 @@ export async function getServerSideProps({ params, res }) {
     })
     try {
         fs.writeFileSync(targetFile, content);
-      } catch (err) {
+    } catch (err) {
         console.error(err);
-      }
+    }
     return {
         props: {
             content
